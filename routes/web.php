05fc8635a7ddfbf84e0fc3d40472ffdc104ba51e
q33566 <qaz57123@gmail.com>
2024-01-23 14:27:53 +0800

    Database Migration & Model Creation & Factory

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Models\JobList;
-use Illuminate\Http\Request;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,13 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('jobList',[
         'heading' => 'Latest Job List',
-        'jobList' => JobList::allJob()
+        'jobList' => Job::all()
     ]);
 });
 // list single job
 Route::get('/jobList/{id}',function($id){
     return view('job',[
-        'job' => JobList::find($id)
+        'job' => Job::find($id)
     ]);
 });
 /*route basic
